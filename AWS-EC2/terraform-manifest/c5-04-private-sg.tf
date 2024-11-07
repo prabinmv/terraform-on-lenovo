cef2244018e346a154cfd83acbfda7f172fb6695
prabinmv <prabinmv@live.in>
2024-11-07 16:10:34 +0530

    private sg

--- AWS-EC2/terraform-manifest/c5-04-private-sg.tf
+++ AWS-EC2/terraform-manifest/c5-04-private-sg.tf
@@ -1,12 +1,12 @@
-module "public_bastion_sg" {
+module "private_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
 
-  name = "public-bastion_sg"
+  name = "private-sg"
   description = "Security group ssh port open for all"
   vpc_id = module.vpc.vpc_id
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules = ["ssh-tcp"]
+  ingress_cidr_blocks = module.vpc.vpc_cidr_block
+  ingress_rules = ["ssh-tcp", "https-443-tcp"]
   egress_rules = ["all-all"]
   tags = local.common_tags
 }
