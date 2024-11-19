db50f33523daab098af3ed04f7cf7fbf62974cd8
prabinmv <prabinmv@live.in>
2024-11-19 10:48:02 +0530

    local values

--- ALB/terraform-manifest/c10-02-ALB-application-loadbalancer.tf
+++ ALB/terraform-manifest/c10-02-ALB-application-loadbalancer.tf
@@ -7,7 +7,7 @@ module "alb" {
   name    = "${local.name}-alb"
   vpc_id  = module.vpc.vpc_id
   subnets = module.vpc.public_subnets
-  security_groups = module.loadbalancer_sg.security_group_id
+  security_groups = [module.loadbalancer_sg.security_group_id]
   tags = local.common_tags
 
   #listerner
