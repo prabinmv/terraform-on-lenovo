5c582d221e319db2813bf28e190cf93a5829a9da
prabinmv <prabinmv@live.in>
2024-11-19 10:21:03 +0530

    alb output

--- ALB/terraform-manifest/c10-02-ALB-application-loadbalancer.tf
+++ ALB/terraform-manifest/c10-02-ALB-application-loadbalancer.tf
@@ -9,4 +9,70 @@ module "alb" {
   subnets = module.vpc.public_subnets
   security_groups = module.loadbalancer_sg.security_group_id
   tags = local.common_tags
+
+  #listerner
+
+  listeners = {
+    my-http-listerner = {
+      port            = 80
+      protocol        = "HTTP"
+    #   certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+      forward = {
+        target_group_key = "mytg1"
+      }
+    }
+  }
+
+  # Target Gtoup
+  target_groups = {
+    mytg1 = {
+      create_attachment = false
+      name_prefix                       = "mytg1-"
+      protocol                          = "HTTP"
+      port                              = 80
+      target_type                       = "instance"
+      deregistration_delay              = 10
+      load_balancing_algorithm_type     = "weighted_random"
+      load_balancing_anomaly_mitigation = "on"
+      load_balancing_cross_zone_enabled = false
+
+      target_group_health = {
+        dns_failover = {
+          minimum_healthy_targets_count = 2
+        }
+        unhealthy_state_routing = {
+          minimum_healthy_targets_percentage = 50
+        }
+      }
+
+      health_check = {
+        enabled             = true
+        interval            = 30
+        path                = "/app1/index.html"
+        port                = "traffic-port"
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 6
+        protocol            = "HTTP"
+        matcher             = "200-399"
+      }
+
+      protocol_version = "HTTP1"
+    #   target_id        = aws_instance.this.id
+      port             = 80
+      tags = local.common_tags
+    } // TG1
+  }
+}
+
+resource "aws_lb_target_group_attachment" "mytg1" {
+  for_each = {for k, v in module.ec2_private: k => v}
+  target_group_arn = module.alb.target_groups["mytg1"].arn
+  target_id        = each.value.id
+  port             = 80
+}
+
+output "zz_ec2_private" {
+  value = {for ec2_instance, details in  module.ec2_private: ec2_instance => details}
 }
