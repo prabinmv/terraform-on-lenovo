ba05e44b80225b3d30a2e40ad9c0988e05c7ec23
prabinmv <prabinmv@live.in>
2024-12-10 12:27:13 +0530

    host header

---
+++ ALB-HostHeader/terraform-manifest/c12-route53-dnsregistration.tf
@@ -0,0 +1,35 @@
+resource "aws_route53_record" "default_dns" {
+  zone_id = data.aws_route53_zone.mydoamin.zone_id
+  name    = "myapps.cloud9aws.in"
+  type    = "A"
+  # ttl     = 300
+  alias {
+    name                   = module.alb.dns_name
+    zone_id                = module.alb.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "app1_dns" {
+  zone_id = data.aws_route53_zone.mydoamin.zone_id
+  name    = var.app1_dns_name
+  type    = "A"
+  # ttl     = 300
+  alias {
+    name                   = module.alb.dns_name
+    zone_id                = module.alb.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "app2_dns" {
+  zone_id = data.aws_route53_zone.mydoamin.zone_id
+  name    = var.app2_dns_name
+  type    = "A"
+  # ttl     = 300
+  alias {
+    name                   = module.alb.dns_name
+    zone_id                = module.alb.zone_id
+    evaluate_target_health = true
+  }
+}
