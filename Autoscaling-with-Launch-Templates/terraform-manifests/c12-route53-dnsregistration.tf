653ef0cefe3d6d536d37150d99ce376fd6224d63
prabinmv <prabinmv@live.in>
2024-12-21 15:06:57 +0530

    launch template

---
+++ Autoscaling-with-Launch-Templates/terraform-manifests/c12-route53-dnsregistration.tf
@@ -0,0 +1,11 @@
+resource "aws_route53_record" "apps_dns" {
+  zone_id = data.aws_route53_zone.mydoamin.zone_id
+  name    = "asg-it.cloud9aws.in"
+  type    = "A"
+  # ttl     = 300
+  alias {
+    name                   = module.alb.dns_name
+    zone_id                = module.alb.zone_id
+    evaluate_target_health = true
+  }
+}
