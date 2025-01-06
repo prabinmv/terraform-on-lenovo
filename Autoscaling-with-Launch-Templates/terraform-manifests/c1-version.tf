3ff4ba3557d7bdac3631a392983a2eeb22bfc8e8
prabinmv <prabinmv@live.in>
2025-01-06 14:33:27 +0530

    random resource

--- Autoscaling-with-Launch-Templates/terraform-manifests/c1-version.tf
+++ Autoscaling-with-Launch-Templates/terraform-manifests/c1-version.tf
@@ -8,10 +8,20 @@ terraform {
       source = "hashicorp/null"
       version = "3.2.3"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.3"
+    }
   }
 }
 
 provider "aws" {
 #   region =
+}
+
+
+# Create Random Pet Resource
+resource "random_pet" "this" {
+  length = 2
 }
 
