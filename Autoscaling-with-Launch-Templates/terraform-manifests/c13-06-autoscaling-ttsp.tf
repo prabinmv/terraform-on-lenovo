a1358cd50357091673d70cffe524b4ee0561a5b6
prabinmv <prabinmv@live.in>
2025-01-06 15:26:43 +0530

    target tracking

--- Autoscaling-with-Launch-Templates/terraform-manifests/c13-06-autoscaling-ttsp.tf
+++ Autoscaling-with-Launch-Templates/terraform-manifests/c13-06-autoscaling-ttsp.tf
@@ -1,5 +1,5 @@
 resource "aws_autoscaling_policy" "avg_cpu_policy_greater_than_50" {
-  name                   = "avg-cpu-policy-greater-than-5"
+  name                   = "avg-cpu-policy-greater-than-50"
   autoscaling_group_name = aws_autoscaling_group.my_asg.id
   estimated_instance_warmup = 180
   policy_type = "TargetTrackingScaling"
@@ -13,4 +13,19 @@ resource "aws_autoscaling_policy" "avg_cpu_policy_greater_than_50" {
   }
 }
 
+resource "aws_autoscaling_policy" "alb_target_request_greater_than_yy" {
+  name                   = "alb-target-request-greater-than-yy"
+  autoscaling_group_name = aws_autoscaling_group.my_asg.id
+  estimated_instance_warmup = 180
+  policy_type = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ALBRequestCountPerTarget"
+      resource_label =  "${module.alb.arn_suffix}/${module.alb.target_groups["mytg1"].arn_suffix}"
+    }
+
+    target_value = 10
+  }
+}
 
