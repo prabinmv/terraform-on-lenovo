b02d4009e06ded0a83633ff0cbd60fdc64181bb1
prabinmv <prabinmv@live.in>
2025-01-06 14:24:42 +0530

    autoscaling

---
+++ Autoscaling-with-Launch-Templates/terraform-manifests/c13-03-autoscaling-resource.tf
@@ -0,0 +1,26 @@
+resource "aws_autoscaling_group" "myasg" {
+  name                      = "myasg"
+  max_size                  = 3
+  min_size                  = 2
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+  desired_capacity          = 2
+  vpc_zone_identifier = module.vpc.private_subnets
+  target_group_arns = [module.alb.target_groups["mytg1"].arn]
+  launch_template {
+    id = aws_launch_template.my_launch_template.id
+    version = aws_launch_template.my_launch_template.latest_version 
+  }
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["desired_capacity"]
+  }
+  tag {
+    key                 = "Owners"
+    value               = "Web-Team"
+    propagate_at_launch = true
+  }
+}
