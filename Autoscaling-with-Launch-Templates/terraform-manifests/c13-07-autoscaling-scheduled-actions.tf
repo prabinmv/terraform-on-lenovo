7d2bf96c7b4540debabd1ddba9b2762370299423
prabinmv <prabinmv@live.in>
2025-01-06 15:33:27 +0530

    schedule action

---
+++ Autoscaling-with-Launch-Templates/terraform-manifests/c13-07-autoscaling-scheduled-actions.tf
@@ -0,0 +1,19 @@
+resource "aws_autoscaling_schedule" "increase_capacity_9am" {
+  scheduled_action_name  = "increase-capacity-9am"
+  min_size               = 2
+  max_size               = 10
+  desired_capacity       = 8
+  start_time             = "2025-12-11T18:00:00Z"
+  recurrence = "00 09 * * *"
+  autoscaling_group_name = aws_autoscaling_group.my_asg.id
+}
+
+resource "aws_autoscaling_schedule" "decrease_capacity_9am" {
+  scheduled_action_name  = "decrease-capacity-9pm"
+  min_size               = 2
+  max_size               = 10
+  desired_capacity       = 2
+  start_time             = "2025-12-11T18:00:00Z"
+  recurrence = "00 21 * * *"
+  autoscaling_group_name = aws_autoscaling_group.my_asg.id
+}
